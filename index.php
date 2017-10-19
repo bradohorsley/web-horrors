2aa76f787a799e3d9db9d8b062a2781e81b68a3a
bradohorsley <bradohorsley@aim.com>
2017-10-19 23:30:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,44 @@
+<h1>This Is A Test</h1>
 <?php
 //connect to database
 require_once('lib/data-connect.php');
 $db = new connection;
 $data = $db->connect();
 
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
 
+if ($data->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $data->error;
+}
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($data->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $data->error;
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $data->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
 
 ?>
